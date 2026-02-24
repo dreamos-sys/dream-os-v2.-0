647e990cb7e6d289412506e4f7861f1813517d03
Dream <afumum234@gmail.com>
2026-02-24 22:52:00 +0700

    Create config.js

---
+++ core/config.js
@@ -0,0 +1,13 @@
+export const CONFIG = {
+    supabase: {
+        url: 'https://rqpodzjexghrvcpyacyo.supabase.co',
+        key: 'sb_publishable_U9MbSdPJOMSmaw3BsHJcVQ_PDiOy-UM'
+    },
+    appName: 'Dream OS v2.0',
+    version: '2.0.0',
+    features: {
+        enableAI: true,
+        enableBackup: true,
+        enableAnalytics: true
+    }
+};
