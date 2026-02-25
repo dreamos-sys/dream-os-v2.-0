40b8086cd46c9b8803f27d8b3b0463de9cb0f09f
Dream <afumum234@gmail.com>
2026-02-25 07:27:10 +0700

    Update config.js

--- core/config.js
+++ core/config.js
@@ -1,11 +1,8 @@
+// core/config.js
 export const CONFIG = {
     supabase: {
         url: 'https://pvznaeppaagylwddirla.supabase.co',
-        key: 'sb_publishable_N1D88DzIb-QhyjQtsxPn_g_SWMxGQcP'
-    },
-    // ... sisanya
-};
-        
+        key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2em5hZXBwYWFneWx3ZGRpcmxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5NTEwNDMsImV4cCI6MjA4NzUyNzA0M30.t9SJi3VfsBDkKmeZ3egZ4rbvljl4xe0WwNkPtfA9-vo'
     },
     appName: 'Dream OS v2.0',
     version: '2.0.0',
