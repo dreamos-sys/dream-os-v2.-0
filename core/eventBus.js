878a7c8d91af43f6913c774c0d46c250fc168cd6
Dream <afumum234@gmail.com>
2026-02-27 05:56:28 +0700

    Create eventBus.js

---
+++ core/eventBus.js
@@ -0,0 +1,8 @@
+export const eventBus = {
+    on(event, callback) {
+        document.addEventListener(event, (e) => callback(e.detail));
+    },
+    emit(event, data) {
+        document.dispatchEvent(new CustomEvent(event, { detail: data }));
+    }
+};
