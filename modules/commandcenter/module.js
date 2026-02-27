cc22171be97e68b341d101fd1d0a8fcd57abd056
Dream <afumum234@gmail.com>
2026-02-27 14:02:38 +0700

    Update module.js

--- modules/commandcenter/module.js
+++ modules/commandcenter/module.js
@@ -1,5 +1,1 @@
-// Minimal module.js untuk memenuhi router Dream OS
-export function init() {
-    console.log('[COMMANDCENTER] Module initialized (dummy)');
-    // Semua logika sudah ada di index.html
-}
+
