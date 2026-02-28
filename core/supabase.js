dcb8921ab4df8a58d90bcdb9d18876ee8c337195
Dream <afumum234@gmail.com>
2026-02-28 23:15:55 +0700

    Update supabase.js

--- core/supabase.js
+++ core/supabase.js
@@ -1,9 +1,3 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 import { CONFIG } from './config.js';
-
-export const supabase = createClient(CONFIG.supabase.url, CONFIG.supabase.key);
-// core/supabase.js
-import { CONFIG } from './config.js';
-
 const { createClient } = window.supabase;
-export const supabase = createClient(CONFIG.SUPABASE_URL, CONFIG.SUPABASE_ANON_KEY);
+export const supabase = createClient(CONFIG.supabase.url, CONFIG.supabase.key);
