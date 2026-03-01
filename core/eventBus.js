c730b71f2fad42b51b650137fe1e9a2137794947
Dream <afumum234@gmail.com>
2026-03-02 00:23:58 +0700

    Update eventBus.js

--- core/eventBus.js
+++ core/eventBus.js
@@ -1,16 +1,6 @@
 const events = {};
-
 export const eventBus = {
-    on(event, callback) {
-        if (!events[event]) events[event] = [];
-        events[event].push(callback);
-    },
-    off(event, callback) {
-        if (!events[event]) return;
-        events[event] = events[event].filter(cb => cb !== callback);
-    },
-    emit(event, data) {
-        if (!events[event]) return;
-        events[event].forEach(cb => cb(data));
-    }
+    on(event, cb) { if (!events[event]) events[event] = []; events[event].push(cb); },
+    off(event, cb) { if (!events[event]) return; events[event] = events[event].filter(c => c !== cb); },
+    emit(event, data) { if (!events[event]) return; events[event].forEach(cb => cb(data)); }
 };
